b1cd7a136e67fa4adaa6b5f6807df97fd8b0780f
MayuRuru <aruizrull@protonmail.com>
2023-03-07 11:09:22 +0100

    Create Order, List and apply styles

---
+++ src/features/orders/Order.js
@@ -0,0 +1,52 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+import { selectOrderById } from "./ordersApiSlice";
+
+import React from "react";
+
+const Order = ({ orderId }) => {
+  const order = useSelector((state) => selectOrderById(state, orderId));
+
+  const navigate = useNavigate();
+
+  if (order) {
+    const created = new Date(order.createdAt).toLocaleString("es-ES", {
+      day: "numeric",
+      month: "long",
+    });
+
+    const updated = new Date(order.updatedAt).toLocaleString("es-ES", {
+      day: "numeric",
+      month: "long",
+    });
+
+    const handleEdit = () => navigate(`/dash/orders/${orderId}`);
+
+    return (
+      <tr className="table__row">
+        <td className="table__cell order__status">
+          {order.completed ? (
+            <span className="order__status--completed">Completed</span>
+          ) : (
+            <span className="order__status--open">Open</span>
+          )}
+        </td>
+        <td className="table__cell order__created">{created}</td>
+        <td className="table__cell order__updated">{updated}</td>
+        <td className="table__cell order__title">{order.title}</td>
+        <td className="table__cell order__username">{order.username}</td>
+
+        <td className="table__cell">
+          <button className="icon-button table__button" onClick={handleEdit}>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </button>
+        </td>
+      </tr>
+    );
+  } else return null;
+};
+
+export default Order;
--- src/features/orders/OrdersList.js
+++ src/features/orders/OrdersList.js
@@ -1,7 +1,58 @@
-import React from "react";
+import { useGetOrdersQuery } from "./ordersApiSlice";
+import Order from "./Order";
 
 const OrdersList = () => {
-  return <h1>Orders</h1>;
+  const {
+    data: orders,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetOrdersQuery;
+
+  let content;
+  if (isLoading) content = <p>Loading...</p>;
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message}</p>;
+  }
+
+  if (isSuccess) {
+    const { ids } = orders;
+
+    const tableContent = ids?.length
+      ? ids.map((orderId) => <Order key={orderId} orderId={orderId} />)
+      : null;
+
+    content = (
+      <table className="table table--orders">
+        <thead className="table__thead">
+          <tr>
+            <th scope="col" className="table__th order__status">
+              Username
+            </th>
+            <th scope="col" className="table__th order__created">
+              Created
+            </th>
+            <th scope="col" className="table__th order__updated">
+              Updated
+            </th>
+            <th scope="col" className="table__th order__title">
+              Title
+            </th>
+            <th scope="col" className="table__th order__username">
+              Owner
+            </th>
+            <th scope="col" className="table__th order__edit">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
+
+  return content;
 };
 
 export default OrdersList;
