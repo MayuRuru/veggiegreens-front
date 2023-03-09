771fc813fac46e671d531ddf70359c34e1d068c3
MayuRuru <aruizrull@protonmail.com>
2023-03-09 11:56:49 +0100

    Add new routes to Notes and Users

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import DashLayout from "./components/DashLayout";
 import Welcome from "./features/auth/Welcome";
 import OrdersList from "./features/orders/OrdersList";
 import UsersList from "./features/users/UsersList";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/orders/EditNote";
+import NewNote from "./features/orders/NewNote";
 
 function App() {
   return (
@@ -18,12 +22,18 @@ function App() {
           {/* From here protected routes: */}
           <Route index element={<Welcome />} />
 
-          <Route path="orders">
-            <Route index element={<OrdersList />} />
-          </Route>
-
+          {/* Users routes: */}
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUserForm />} />
+          </Route>
+
+          {/* Orders routes: */}
+          <Route path="orders">
+            <Route index element={<OrdersList />} />
+            <Route path=":id" element={<EditNote />} />
+            <Route path="new" element={<NewNote />} />
           </Route>
         </Route>
         {/*End Dash - Private routes*/}
