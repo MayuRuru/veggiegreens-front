e2e3e44028e6f82883c57912a60a2c5b5995a233
MayuRuru <aruizrull@protonmail.com>
2023-03-11 12:08:34 +0100

    Create authApiSlice

---
+++ src/features/auth/authApiSlice.js
@@ -0,0 +1,46 @@
+import { apiSlice } from "../../app/api/apiSlice";
+import { logOut } from "./authSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    // LOGIN
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+
+    // SEND LOGOUT
+    sendLogout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+      // RTKQuery function provides dispatch and veryification for the query
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          console.log(data);
+          dispatch(logOut());
+          //apiSlice need to be cache cleared too:
+          dispatch(apiSlice.util.resetApiState());
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    }),
+
+    // REFRESH
+    refresh: builder.mutation({
+      query: () => ({
+        url: "/auth/refresh",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useSendLogoutMutation, useRefreshMutation } =
+  authApiSlice;
