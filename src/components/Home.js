570259a1e6b2453ef5054ad5860ca414a3146098
MayuRuru <aruizrull@protonmail.com>
2023-05-10 10:39:56 +0200

    Render recipes or not found and input form in Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,42 @@
 import { Link } from "react-router-dom";
-
 import React from "react";
+import veggieRecipes from "../mocks/veggie-recipes.json";
+//import noData from "../mocks/no-results.json";
 
 const Home = () => {
+  const recipes = veggieRecipes.results;
+  //console.log(recipes);
+  const hasRecipes = recipes.length > 0;
+
+  const renderRecipes = () => {
+    return (
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <img src={recipe.image} alt={recipe.title} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderNoResults = () => {
+    return <p>No recipes found</p>;
+  };
+
   const content = (
     <section className="home">
       <header>
         <h1>Welcome</h1>
+        <form>
+          <input placeholder="Spaguetti aglio et olio..." />
+          <button type="submit">Find a recipe</button>
+        </form>
       </header>
 
       <main className="home__main">
+        <section>{hasRecipes ? renderRecipes : renderNoResults}</section>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
